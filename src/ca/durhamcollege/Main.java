004f0e5ddf837af93585016ccc9f64995e3b0351
Nick Sturch-Flint <nick.rulez@live.com>
2020-11-04 11:06:23 -0500

    Initial commit

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,12 @@
+package ca.durhamcollege;
+
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        out.println("Hello World!");
+    }
+}
